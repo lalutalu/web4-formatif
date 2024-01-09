78c93f99334b4309af931a6103d95a3310af5885
Lukas Alutalu <121912040+lalutalu@users.noreply.github.com>
2024-01-08 21:49:46 -0500

    webapi – ajout de swagger

--- 1.1/src/Web2.API/Controllers/ProductsController.cs
+++ 1.1/src/Web2.API/Controllers/ProductsController.cs
@@ -1,11 +1,17 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using System.Text;
+using System;
 using Web2.API.Models;
+using System.Net;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Web2.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Produces("application/json")]
     public class ProductsController : ControllerBase
     {
         static List<Product> products = new List<Product>()
@@ -16,12 +22,50 @@ namespace Web2.API.Controllers
         };
 
 
+        /// <summary>
+        /// Obtient la liste de tous les produits
+        /// </summary>
+        /// <returns>La liste complète des Produits</returns>
+        /// <response code="404">La liste est introuvable</response>
         [HttpGet]
         public ActionResult<IEnumerable<Product>> Get()
         {
             return Ok(products);
         }
 
+        /// <summary>
+        /// Crée un nouveau produit
+        /// </summary>
+        /// <param name="product">Le produit à créer</param>
+        /// <returns>La liste des produits avec le produit nouvellement crée.</returns>
+        /// <response code="201">Produit créer avec succès
+        /// <para>
+        /// Action réussie.L’URI de la nouvelle ressource est inclus dans
+        /// l’en-tête Location de la réponse.Le corps de la réponse
+        /// contient une représentation de la ressource
+        /// </para>
+        /// </response>
+        /// <response code="200">
+        /// <para>
+        /// La méthode effectue des opérations de traitement, mais ne 
+        /// crée pas de ressource et inclut le résultat de l’opération dans le
+        /// corps de la réponse.
+        /// </para>
+        /// </response>
+        /// <response code="204">Produit créer avec succès
+        /// <para>
+        /// La méthode effectue des opérations de traitement, mais ne
+        /// crée pas de ressource et ne retourne pas de contenue dans le
+        /// corps de la réponse
+        /// </para>
+        /// </response>
+        /// <response code="400">Produit créer avec succès
+        /// <para>
+        /// données non valides dans la requête.Le corps de la réponse
+        /// peut contenir des informations supplémentaires sur l’erreur ou
+        /// un lien vers un URI qui fournit plus de détails
+        /// </para>
+        /// </response>
         [HttpPost]
         public ActionResult<Product> CreateProduct([FromBody] Product product)
         {
@@ -37,6 +81,20 @@ namespace Web2.API.Controllers
             return Ok(products);
 
         }
+
+        /// <summary>
+        /// Supprime un produit
+        /// </summary>
+        /// <param name="id">L'identifiant du produit a effacer</param>
+        /// <returns>La liste des produits sans le produit nouvellement effacé</returns>
+        /// <response code="200">
+        /// <para>
+        /// La méthode effectue des opérations de traitement, mais ne 
+        /// crée pas de ressource et inclut le résultat de l’opération dans le
+        /// corps de la réponse.
+        /// </para>
+        /// </response>
+        /// <response code="404">La ressource est introuvable</response>
 
         [HttpDelete("{id}")]
         public ActionResult<Product> DeleteProduct(int id)
--- 1.1/src/Web2.API/Program.cs
+++ 1.1/src/Web2.API/Program.cs
@@ -1,7 +1,50 @@
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Formatif 1",
+        Description = "Premier Formatif du Cours Web4",
+        TermsOfService = new Uri("https://example.com/terms"),
+        Contact = new OpenApiContact
+        {
+            Name = "Lukas Alutalu",
+            Url = new Uri("https://github.com/lalutalu/")
+        },
+        License = new OpenApiLicense
+        {
+            Name = "Example License",
+            Url = new Uri("https://example.com/license")
+        }
+    });
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+});
+
 
 var app = builder.Build();
+
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    options.RoutePrefix = string.Empty;
+});
 
 app.MapControllers();
 
